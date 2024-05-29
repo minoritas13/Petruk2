9e8a3a3d6863f25d6d1bc3d7f5d535a06e747105
Nafis Rizqullah <148970879+minoritas13@users.noreply.github.com>
2024-05-29 13:23:53 +0700

    penambahan dan pembenaran kode

--- Tugaspetruk.cpp
+++ Tugaspetruk.cpp
@@ -9,7 +9,7 @@ bool isOperator(char c){
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
 }
 
-int identifikasi(char op){
+int precedence(char op){
     switch (op)
     {
     case '+':
@@ -24,25 +24,32 @@ int identifikasi(char op){
     }
 }
 
-vector<string> infifTopostfix(const vector<string>& token){
+vector<string> infixToPostfix(const vector<string>& tokens) {
     vector<string> output;
     stack<string> operators;
 
-    for(const auto& token : token){
-        if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)){
+    for (const auto& token : tokens) {
+        if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)) {
             output.push_back(token);
-        }else if( token == "()"){
+        } else if (token == "(") {
             operators.push(token);
-        }else if( token == ")"){
-            while (!operators.empty() && operators.top() != "()"){
+        } else if (token == ")") {
+            while (!operators.empty() && operators.top() != "(") {
                 output.push_back(operators.top());
                 operators.pop();
             }
-            operators.pop();
-        }else if (isOperator(token[0])){
-            while(!operators.empty() && precedence(operators.top()[0]) >= precedence(token[0])){
-
+            operators.pop(); // Remove '('
+        } else if (isOperator(token[0])) {
+            while (!operators.empty() && precedence(operators.top()[0]) >= precedence(token[0])) {
+                output.push_back(operators.top());
+                operators.pop();
             }
+            operators.push(token);
         }
+    }
+
+    while(!operators.empty()){
+        output.push_back(operators.top());
+        operators.pop();
     }
 }
